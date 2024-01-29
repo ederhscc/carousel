9429ab9bcdc03056f44dfc07553243a4510c360b
Eder Henrique <ederhscc@gmail.com>
2024-01-29 16:33:58 -0300

    added functions for to change the images

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,10 +1,34 @@
 import { useState } from "react";
 import Slide from "./Slide";
 import Indicators from "./Indicators";
+import { useEffect } from "react";
 
 const Carousel = ({ imageUrls }) => {
   const [activeIndex, setActiveIndex] = useState(0);
   const [manualChange, setManualChange] = useState(false);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (!manualChange) {
+        setActiveIndex((prevIndex) => (prevIndex + 1) % imageUrls.length);
+      }
+      setManualChange(false)
+    }, 5000);
+
+    return () => clearInterval(interval);
+  }, [manualChange, imageUrls.length]);
+
+  const goPrev = () => {
+    setManualChange(true);
+    setActiveIndex(
+      (prevIndex) => (prevIndex - 1 + imageUrls.length) % imageUrls.length
+    );
+  };
+
+  const goNext = () => {
+    setManualChange(true);
+    setActiveIndex((prevIndex) => (prevIndex + 1) % imageUrls.length);
+  };
 
   return (
     <div className="carousel">
@@ -12,8 +36,12 @@ const Carousel = ({ imageUrls }) => {
         <Slide key={index} url={url} isActive={activeIndex === index} />
       ))}
       <Indicators activeIndex={activeIndex} length={imageUrls.length} />
-      <button className="carousel-button prev">Anterior</button>
-      <button className="carousel-button next">Próximo</button>
+      <button className="carousel-button prev" onClick={goPrev}>
+        Anterior
+      </button>
+      <button className="carousel-button next" onClick={goNext}>
+        Próximo
+      </button>
     </div>
   );
 };
