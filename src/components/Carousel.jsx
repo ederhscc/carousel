b90672038ca11c869cf9053cab967042a79dbd7c
Eder Henrique <ederhscc@gmail.com>
2024-01-29 16:15:53 -0300

    added project's styles

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,19 +1,19 @@
-import {useState} from "react";
+import { useState } from "react";
 import Slide from "./Slide";
 import Indicators from "./Indicators";
 
-const Carousel = ({imageUrls}) => {
-    const [activeIndex, setActiveIndex] = useState(0)
-    const [manualChange, setManualChange] = useState(false)
+const Carousel = ({ imageUrls }) => {
+  const [activeIndex, setActiveIndex] = useState(0);
+  const [manualChange, setManualChange] = useState(false);
 
   return (
     <div className="carousel">
       {imageUrls.map((url, index) => (
-        <Slide key={index} url={url} isActive={activeIndex === index}/>
+        <Slide key={index} url={url} isActive={activeIndex === index} />
       ))}
-      <Indicators activeIndex={activeIndex} length={imageUrls.length}/>
-      <button>Anterior</button>
-      <button>Próximo</button>
+      <Indicators activeIndex={activeIndex} length={imageUrls.length} />
+      <button className="carousel-button prev">Anterior</button>
+      <button className="carousel-button next">Próximo</button>
     </div>
   );
 };
