b28e27120c9c3922393217fc27c9f24646ea13de
Eder Henrique <ederhscc@gmail.com>
2024-01-29 15:10:21 -0300

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Carousel from './components/Carousel'
+
+function App() {
+  
+  return (
+    <>
+      <h1>Carrossel</h1>
+      <Carousel />
+    </>
+  )
+}
+
+export default App
